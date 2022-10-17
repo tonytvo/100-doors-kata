38563f19c3ecafddca1dc820575dd4db83c8aebe
Tony Vo <ttrung.vo@gmail.com>
2022-10-16 18:10:44 -0600

    initial commits

---
+++ src/main/java/com/hundreddoors/HundredDoors.java
@@ -0,0 +1,4 @@
+package com.hundreddoors;
+
+class HundredDoors {
+}
---
+++ src/test/java/com/hundreddoors/HundredDoorsTest.java
@@ -0,0 +1,16 @@
+package com.hundreddoors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class HundredDoorsTest {
+
+    @Test
+    public void foo() {
+        HundredDoors app = new HundredDoors();
+        //app.findSolutions();
+        fail();
+    }
+
+}
